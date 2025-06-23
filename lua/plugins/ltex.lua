ab672bbb40f7597c14e71ea98bf1d9f85fda9aa1
Eval-99 <ev1914jw@gmail.com>
2025-06-23 14:25:28 -0500

    Added extra Ltex functions and reconfigured snippets a little.

---
+++ lua/plugins/ltex.lua
@@ -0,0 +1,11 @@
+return {
+  'barreiroleo/ltex_extra.nvim',
+  branch = 'dev',
+  opts = {
+    server_opts = nil,
+    log_level = 'none',
+    init_check = true,
+    load_langs = { 'en-US', 'es' },
+    path = '~/.config/nvim/stardict/ltex',
+  },
+}
--- lua/snippets/latex.lua
+++ lua/snippets/latex.lua
@@ -1,14 +1,4 @@
-local ls = require('luasnip')
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
-local extras = require('luasnip.extras')
-local rep = extras.rep
-local fmt = require('luasnip.extras.fmt').fmt
-local c = ls.choice_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local sn = ls.snippet_node
+local ls, s, t, i, extras, rep, fmt, c, f, d, sn = unpack(require('snippets._snip-init'))
 
 ls.add_snippets('tex', {
   s('beg', {
@@ -90,12 +80,12 @@ ls.add_snippets('tex', {
       ]],
       {
         i(1, 'Elías Valdés'), -- \author
-        rep(1), -- \title
-        i(2, 'Version 0.0'), -- \date first
+        i(2, 'Title'), -- \author
+        i(3, 'Version 0.0'), -- \date first
         f(function() -- \date second (current date)
           return os.date('%Y-%m-%d')
         end, {}),
-        i(3, 'Section Title'), -- \section
+        i(4, 'Section Title'), -- \section
       }
     )
   ),
