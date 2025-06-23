68c1f196c6eaab416c28040c5aebf0c55afab8c7
Eval-99 <ev1914jw@gmail.com>
2025-06-22 21:54:53 -0500

    Another small wiki.vim config change.

--- lua/plugins/wiki.lua
+++ lua/plugins/wiki.lua
@@ -1,6 +1,27 @@
 return {
   'lervag/wiki.vim',
   config = function()
+    vim.keymap.set('n', '<leader>wc', function()
+      local line = vim.api.nvim_get_current_line()
+      local states = { '[ ]', '[x]', '[>]', '[~]', '[!]' }
+      local s, e = line:find('%[.?.?%]')
+
+      if s and e then
+        local current = line:sub(s, e)
+        local next_state = '[ ]' -- default if not found
+
+        for i, state in ipairs(states) do
+          if state == current then
+            next_state = states[(i % #states) + 1]
+            break
+          end
+        end
+
+        local new_line = line:sub(1, s - 1) .. next_state .. line:sub(e + 1)
+        vim.api.nvim_set_current_line(new_line)
+      end
+    end)
+
     vim.cmd([[
     let g:wiki_root = '~/Documents/Notes/zk'
     let g:wiki_link_creation = {
