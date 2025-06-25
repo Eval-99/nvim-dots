f89dfff10b783315dfb974f04bb257a1dcd8229a
Eval-99 <ev1914jw@gmail.com>
2025-06-25 01:31:11 -0500

    Terminal mode fix. nvim-dap no longer enters insert mode when started... For real this time...

--- lua/plugins_local/terminal.lua
+++ lua/plugins_local/terminal.lua
@@ -1,13 +1,12 @@
 -- Create vertical terminal and cd to cwd.
 vim.api.nvim_create_user_command('TermVert', function()
   vim.cmd('vsplit | lcd %:p:h | terminal')
-  vim.cmd('startinsert')
+  vim.b.from_term_command = true
 end, {})
 
--- Create horizontal terminal, resize to 10, and cd to cwd.
 vim.api.nvim_create_user_command('TermHori', function()
   vim.cmd('split | resize 10 | lcd %:p:h | terminal')
-  vim.cmd('startinsert')
+  vim.b.from_term_command = true
 end, {})
 
 -- Keymaps to open terminals
@@ -16,6 +15,18 @@ vim.keymap.set('n', '<Leader>th', ':TermHori<CR>', { desc = 'Open horizontal ter
 
 -- Use C-n to enter the terminal normal mode.
 vim.keymap.set('t', '<ESC><ESC>', '<C-\\><C-n>', { desc = 'Enter terminal normal mode' })
+
+-- Start terminal in insert mode.
+vim.api.nvim_create_autocmd({ 'TermOpen', 'WinEnter' }, {
+  group = vim.api.nvim_create_augroup('terminal_insert', { clear = true }),
+  callback = function()
+    vim.defer_fn(function()
+      if vim.bo.buftype == 'terminal' and vim.b.from_term_command then
+        vim.cmd('startinsert')
+      end
+    end, 10)
+  end,
+})
 
 -- Set number and relativenumber when entering terminal normal mode.
 vim.api.nvim_create_autocmd({ 'TermLeave' }, {
