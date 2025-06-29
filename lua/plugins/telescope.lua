803c49c81891332be6e233a42e368f146e9b29dc
Eval-99 <ev1914jw@gmail.com>
2025-06-29 12:37:40 -0500

    Changed some keymaps and added a Nerd font icon picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
     { 'nvim-telescope/telescope-ui-select.nvim' },
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     { 'allaman/emoji.nvim', opts = {} },
+    { '2kabhishek/nerdy.nvim', dependencies = { 'folke/snacks.nvim' } },
   },
   config = function()
     -- Help menu Insert mode: <c-/>
@@ -63,8 +64,9 @@ return {
     })
 
     pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'emoji')
     pcall(require('telescope').load_extension, 'ui-select')
+    pcall(require('telescope').load_extension, 'nerdy')
+    pcall(require('telescope').load_extension, 'emoji')
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
@@ -75,10 +77,12 @@ return {
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch Current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sp', builtin.resume, { desc = '[S]earch [P]ickers' })
+    vim.keymap.set('n', '<leader>sp', builtin.resume, { desc = '[S]earch Open [P]ickers' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Search buffers' })
     vim.keymap.set('n', '<leader>se', require('telescope').load_extension('emoji').emoji, { desc = '[S]earch [E]moji' })
+    vim.keymap.set('n', '<leader>sm', '<CMD>Emoji kaomoji-by-group<CR>', { desc = '[S]earch Kao[m]oji' })
+    vim.keymap.set('n', '<leader>sc', '<CMD>Telescope nerdy<CR>', { desc = '[S]earch Nerd Font [C]haracter' })
 
     vim.keymap.set('n', '<leader>/', function()
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
