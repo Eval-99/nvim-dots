99087da677e3b16ad2c96f176ef3eea6e39708bf
Eval-99 <ev1914jw@gmail.com>
2025-11-03 21:08:11 -0600

    Changed some comments and moved some code blocks around.

--- lua/plugins_local/terminal.lua
+++ lua/plugins_local/terminal.lua
@@ -1,19 +1,20 @@
+-- Create horizontal terminal and cd to cwd.
+vim.api.nvim_create_user_command('TermHori', function()
+  vim.cmd('split | resize 10 | lcd %:p:h | terminal')
+  vim.b.from_term_command = true
+end, {})
+
 -- Create vertical terminal and cd to cwd.
 vim.api.nvim_create_user_command('TermVert', function()
   vim.cmd('vsplit | lcd %:p:h | terminal')
   vim.b.from_term_command = true
 end, {})
 
-vim.api.nvim_create_user_command('TermHori', function()
-  vim.cmd('split | resize 10 | lcd %:p:h | terminal')
-  vim.b.from_term_command = true
-end, {})
-
 -- Keymaps to open terminals
-vim.keymap.set('n', '<Leader>tv', ':TermVert<CR>', { desc = 'Open vertical terminal', silent = true })
 vim.keymap.set('n', '<Leader>th', ':TermHori<CR>', { desc = 'Open horizontal terminal', silent = true })
+vim.keymap.set('n', '<Leader>tv', ':TermVert<CR>', { desc = 'Open vertical terminal', silent = true })
 
--- Use C-n to enter the terminal normal mode.
+-- Use Esc Esc to enter the terminal normal mode.
 vim.keymap.set('t', '<ESC><ESC>', '<C-\\><C-n>', { desc = 'Enter terminal normal mode' })
 
 -- Start terminal in insert mode.
